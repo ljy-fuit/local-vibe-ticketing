1912c360f75d58913dc948e17abe5d8f894e7fd5
이재영 <dlwodud0690@fu-it.com>
2026-02-10 10:48:11 +0900

    Fix: 바인딩 수정

--- src/main.ts
+++ src/main.ts
@@ -35,7 +35,7 @@ async function bootstrap() {
   SwaggerModule.setup('docs', app, document);
 
   const port = process.env.PORT || 46680;
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
   console.log(`Ticketing API is running on port ${port}`);
 }
 bootstrap();
